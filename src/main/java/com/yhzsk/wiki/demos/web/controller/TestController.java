d3aa8645c35878767d538cfe3500bc3672f3fd8b
xiaoheng66 <65148497@qq.com>
2024-07-10 15:27:59 +0800

    增加了自定义配置项的示例

--- src/main/java/com/yhzsk/wiki/demos/web/controller/TestController.java
+++ src/main/java/com/yhzsk/wiki/demos/web/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.yhzsk.wiki.demos.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:tt1}")
+    private String testHello;
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello!!!";
+        return "Hello!!!" + testHello;
     }
 
 
