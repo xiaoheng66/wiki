207820617da6a729707f97f19c417a43499af287
xiaoheng66 <65148497@qq.com>
2024-07-10 11:55:12 +0800

    增加.http 测试接口

--- src/main/java/com/yhzsk/wiki/demos/web/controller/TestController.java
+++ src/main/java/com/yhzsk/wiki/demos/web/controller/TestController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
-
     @RequestMapping("/hello")
     public String hello(){
         return "Hello!!!";
     }
+
+
 
 }
