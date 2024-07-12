73db4858a55b9d383d82360d20d3917f0020ee5d
xiaoheng66 <65148497@qq.com>
2024-07-12 10:52:41 +0800

    封装了单个对象复制和列表复制的功能

--- src/main/java/com/yhzsk/wiki/service/Ebookservice.java
+++ src/main/java/com/yhzsk/wiki/service/Ebookservice.java
@@ -5,6 +5,7 @@ import com.yhzsk.wiki.domain.EbookExample;
 import com.yhzsk.wiki.mapper.EbookMapper;
 import com.yhzsk.wiki.req.EbookReq;
 import com.yhzsk.wiki.resp.EbookResp;
+import com.yhzsk.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,7 @@ public class Ebookservice {
 
         // 原本是直接return 现在改造成这样  用一个Ebook集合来接收
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+       /*
         //创建一个EbookResp集合
         List<EbookResp> respList = new ArrayList<>();
         //在循环中把第一个集合中的转化到第二个  实现实体类的复制
@@ -39,6 +41,11 @@ public class Ebookservice {
             BeanUtils.copyProperties(ebook,ebookResp);
             respList.add(ebookResp);
         }
+
         return respList;
+        */
+
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
