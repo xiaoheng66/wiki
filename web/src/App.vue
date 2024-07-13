daca3065e3955566e3a094e42fc26807983ff115
xiaoheng66 <65148497@qq.com>
2024-07-13 17:49:37 +0800

    集成了Ant Design组件库

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <nav>
+  <div id="nav">
+
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </div>
   <router-view/>
 </template>
 
@@ -15,16 +16,16 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,7 +10,7 @@ import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
+  name: 'Home',
   components: {
     HelloWorld,
   },
